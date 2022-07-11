16107b989c092ae3e61d95cf7c45dce4485399da
迷渡 <justjavac@gmail.com>
2022-07-11 09:42:25 +0800

    fetch pkg's versions
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- routes/[pkg].tsx
+++ routes/[pkg].tsx
@@ -1,7 +1,26 @@
 /** @jsx h */
-import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import versionInfo from "version_info";
 
-export default function Pkg(props: PageProps) {
-  return <div>Hello {props.params.pkg}</div>;
+export const handler: Handlers<string[] | null> = {
+  async GET(_, ctx) {
+    const { pkg } = ctx.params;
+    const versions = await versionInfo(pkg);
+    if (versions == null) {
+      return ctx.render(null);
+    }
+    return ctx.render(versions.versions);
+  },
+};
+
+export default function Pkg({ data, params }: PageProps<string[] | null>) {
+  console.log(data, params.pkg)
+
+  if (!data) {
+    return <h1>The module '{params.pkg}' does not exist</h1>;
+  }
+
+  return <div>Hello {data}</div>;
 }
